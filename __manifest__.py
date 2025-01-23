66dd3ccbe21e2af8f29170b18c4d0123e03ee5dd
sajjatdev <miskatur2002@icloud.com>
2025-01-23 11:57:36 +0600

    add

--- __manifest__.py
+++ __manifest__.py
@@ -6,13 +6,13 @@
     "category": "Pfau/Pfau",
     "summary": "",
     "description": """""",
-    "depends": ["base",'contacts',"web", "sale", "purchase", "stock", "l10n_din5008"],
+    "depends": ["base", "contacts", "web", "sale", "purchase", "stock", "l10n_din5008"],
     "data": [
         "views/sale_views/inherit_sale_form_view.xml",
-        "report/sale_quotation.xml",
-        'views/sale_views/inherit_attribute_form_view.xml',
-        'views/partner_views/partner_view.xml',
-        'views/purchase_order_views/purchase_order.view.xml',
+        "report/report.xml",
+        "views/sale_views/inherit_attribute_form_view.xml",
+        "views/partner_views/partner_view.xml",
+        "views/purchase_order_views/purchase_order.view.xml",
     ],
     "assets": {
         "web.report_assets_common": [
