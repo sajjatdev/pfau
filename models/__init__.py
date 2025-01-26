e73bc83e463ca1b63ef0b19341351cbcfb7231ef
sajjatdev <miskatur2002@icloud.com>
2025-01-26 14:24:19 +0600

    add

--- models/__init__.py
+++ models/__init__.py
@@ -6,3 +6,4 @@ from . import inherit_stock_rule
 from . import inherit_purchase_order
 from . import inherit_account_move_line
 from . import inherit_purchase_order_line
+from . import inherit_account_move
---
+++ models/inherit_account_move.py
@@ -0,0 +1,9 @@
+from odoo import api,fields,models,_
+
+class InheritAccountMove(models.Model):
+    _inherit ='account.move'
+
+    def invoice_note(self):
+        return _(
+            " If the payment term is exceeded, 1.5% per month will be charged. The goods remain our property until full payment is received."
+        )
